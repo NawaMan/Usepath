02f5611ae56a60705605ac14e4f6fe90c5eea6f6
Nawa Manusitthipol <nawa@nawaman.net>
2022-01-03 22:56:08 -0500

    Update license.

--- src/main/java/net/nawaman/usepath/UsableStorage.java
+++ src/main/java/net/nawaman/usepath/UsableStorage.java
@@ -1,3 +1,20 @@
+/*----------------------------------------------------------------------------------------------------------------------
+ * Copyright (C) 2008-2021 Nawapunth Manusitthipol. Implements with and for Java 11 JDK.
+ *----------------------------------------------------------------------------------------------------------------------
+ * LICENSE:
+ * 
+ * This file is part of Nawa's SimpleCompiler.
+ * 
+ * The project is a free software; you can redistribute it and/or modify it under the SIMILAR terms of the GNU General
+ * Public License as published by the Free Software Foundation; either version 2 of the License, or any later version.
+ * You are only required to inform me about your modification and redistribution as or as part of commercial software
+ * package. You can inform me via nawa<at>nawaman<dot>net.
+ * 
+ * The project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ * ---------------------------------------------------------------------------------------------------------------------
+ */
 package net.nawaman.usepath;
 
 import java.io.IOException;
